72379574acf2fa2b3d70f2dfaa3d7587e7ac7ed2
Farhan Rasyid Mustaqim <farhanID0105@gmail.com>
2025-05-08 19:22:14 +0700

    Slinkedlist15

--- singlinklist1.cpp
+++ singlinklist1.cpp
@@ -124,6 +124,7 @@ int main()
         cout << "1. Menambah data kedalam list" << endl;
         cout << "2. Menghapus data dari dalam list" << endl;
         cout << "3. Menampilkan semua data didalam list" << endl;
+        cout << "4. Mencari data dalam list" << endl;
         
     } while ({condition});
     
